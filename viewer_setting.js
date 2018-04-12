9857adc9130e81dafcc106624cb9b02baace7830
Meowming <demingz1@uci.edu>
2018-04-11 17:53:09 -0700

    Add files via upload

---
+++ viewer_setting.js
@@ -0,0 +1,13 @@
+var viewer=new Cesium.Viewer("cesiumContainer",
+{
+    animation:!1,
+    timeline:!1,
+    fullscreenButton:!1,
+    vrButton:!1,
+    geocoder:!1,
+    sceneModePicker:!1,
+    homeButton:!0,
+    navigationHelpButton:!1,
+    baseLayerPicker:!0,
+});
+viewer.cesiumWidget.creditContainer.style.display="none";
